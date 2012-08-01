66ffedf2bbd4de72d75a0478c74e6912d9a9c8ee
pyw <yuwei5@staff.sina.com.cn>
2012-08-01 10:13:27 +0800

    apply change in pycassa version 1.7.0(remove c10)
    Change-Id: I69351dc4b56d17e475cfbba928dc6efca462e212

--- kanyun/database/cassadb.py
+++ kanyun/database/cassadb.py
@@ -18,6 +18,7 @@
 
 import pycassa
 
+
 class CassaDb():
 
     def __init__(self, keyspace='data', ip='127.0.0.1'):
@@ -29,22 +30,22 @@ class CassaDb():
         """
         self.cfs = dict()
         self.db = pycassa.ConnectionPool(keyspace, server_list=[ip])
-        
+
     def insert(self, cf_str, key, values):
         """insert(key, columns[, timestamp][, ttl][, write_consistency_level])
         cf: ColumnFamily String"""
         cf = self.get_cf(cf_str)
         if cf is None or key is None or values is None:
             return
-        
+
         cf.insert(key, values)
-        
+
     def get_range(self, cf_str, row_count=None):
         """get_range([start][, finish][, columns][, column_start][, column_finish]
         [, column_reversed][, column_count][, row_count][, include_timestamp]
         [, super_column][, read_consistency_level][, buffer_size][, filter_empty])"""
         rs = None
-        
+
         cf = self.get_cf(cf_str, new=True)
         if not cf is None:
             try:
@@ -52,18 +53,18 @@ class CassaDb():
                     rs = list(cf.get_range())
                 else:
                     rs = list(cf.get_range(row_count=row_count))
-            except pycassa.cassandra.c10.ttypes.NotFoundException:
+            except pycassa.cassandra.ttypes.NotFoundException:
                 pass
-            
+
         return rs
-    
+
     def get_range2(self, cf_str, row_count=None):
         pool = pycassa.ConnectionPool("data")
         cf = pycassa.ColumnFamily(pool, cf_str)
         return cf.get_range()
-        
-    def get(self, cf_str, key, super_column, 
-            column_start, column_finish, column_count = 20000):
+
+    def get(self, cf_str, key, super_column,
+            column_start, column_finish, column_count=20000):
         """get(key[, columns][, column_start][, column_finish][, column_count]
         [, column_reversed][, include_timestamp][, super_column][, read_consistency_level])
         """
@@ -72,17 +73,17 @@ class CassaDb():
         cf = self.get_cf(cf_str)
         if cf is None or key is None or super_column is None or column_start is None or column_finish is None:
             return rs
-        
+
         try:
             rs = cf.get(key=key, super_column=super_column, column_start=column_start, column_finish=column_finish, column_count=column_count)
-        except pycassa.cassandra.c10.ttypes.NotFoundException:
+        except pycassa.cassandra.ttypes.NotFoundException:
             pass
-        except pycassa.cassandra.c10.ttypes.InvalidRequestException:
+        except pycassa.cassandra.ttypes.InvalidRequestException:
             print 'cassandra db InvalidRequest.'
             pass
-            
+
         return rs
-    
+
     def getbykey(self, cf_str, key):
         """get(key[, columns][, column_start][, column_finish][, column_count]
         [, column_reversed][, include_timestamp][, super_column][, read_consistency_level])
@@ -91,14 +92,14 @@ class CassaDb():
         cf = self.get_cf(cf_str)
         if cf is None or key is None:
             return rs
-        
+
         try:
             rs = cf.get(key=key)
-        except pycassa.cassandra.c10.ttypes.NotFoundException:
+        except pycassa.cassandra.ttypes.NotFoundException:
             pass
-            
+
         return rs
-        
+
     def getbykey2(self, cf_str, key, super_column, column_count):
         """get(key[, columns][, column_start][, column_finish][, column_count]
         [, column_reversed][, include_timestamp][, super_column][, read_consistency_level])
@@ -107,23 +108,22 @@ class CassaDb():
         cf = self.get_cf(cf_str)
         if cf is None or key is None or super_column is None or column_count is None:
             return None
-            
+
         try:
             rs = cf.get(key=key, super_column=super_column, column_count=column_count)
-        except pycassa.cassandra.c10.ttypes.NotFoundException:
+        except pycassa.cassandra.ttypes.NotFoundException:
             pass
-            
+
         return rs
-        
+
     ########### private #########################################
     def get_cf(self, cf_str, new=False):
         """[private]"""
         if new:
             return pycassa.ColumnFamily(self.db, cf_str)
-        if not self.cfs.has_key(cf_str):
+        if not cf_str in self.cfs:
             try:
                 self.cfs[cf_str] = pycassa.ColumnFamily(self.db, cf_str)
-            except pycassa.cassandra.c10.ttypes.NotFoundException:
+            except pycassa.cassandra.ttypes.NotFoundException:
                 return None
         return self.cfs[cf_str]
-
